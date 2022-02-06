42667f65c6594a920e3ba8c40f840b8fdb3d86f3
Nicolás Mariángel Toledo <nicolas.mariangel@usach.cl>
2022-02-06 20:46:13 -0300

    Weird bullets physics

--- Bullet.lua
+++ Bullet.lua
@@ -8,10 +8,17 @@ function Bullet:init(x, y, width, height, direction)
     self.height = height
     self.dx = 30     -- speed x axis
     self.direction = direction
+    self.collider = world:newRectangleCollider(self.x, self.y, self.width, self.height)
 end
 
 function Bullet:update(dt)
-    self.x = self.x + self.direction * self.dx * dt
+    local vx = self.dx * self.direction
+
+    self.collider:setLinearVelocity(vx, -10)
+    self.x = math.floor(self.collider:getX())
+    self.y = math.floor(self.collider:getY())
+
+    -- self.x = self.x + self.direction * self.dx * dt
 end
 
 function Bullet:render()
