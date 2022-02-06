03a6a8ef4edf80bed8704a51d4d073499e206643
Gonzalo Estay <gonzalo.estay@usach.cl>
2022-02-06 03:30:51 -0300

    feat: Adds camera movement and gravity
    Adds a map and camera movement, it looks choppy but that's because
    movement happens in increment of integers, not floats.
    
    Also adds gravity, so the player is constantly falling. Unless you
    press the w key :)

--- Player.lua
+++ Player.lua
@@ -1,16 +1,20 @@
 -- Player class, manages player logic duh
 Player = Class{}
 
+GRAVITY = 4
+
 function Player:init(x, y, width, height)
     self.x = x      -- position x axis
     self.y = y      -- position y axis
     self.width = width  
     self.height = height
-    self.dy = 20     -- speed y axis
-    self.dx = 20     -- speed x axis
+    self.dy = 5     -- speed y axis
+    self.dx = 30     -- speed x axis
 end
 
 function Player:update(dt)
+    self.dy = self.dy + GRAVITY * dt
+    
     if love.keyboard.isDown("left", "a") then
         self.x = self.x - (self.dx * dt);
     elseif love.keyboard.isDown("right", "d") then
@@ -18,10 +22,13 @@ function Player:update(dt)
     end
 
     if love.keyboard.isDown("up", "w") then
+        self.dy = -1
         self.y = self.y - self.dy * dt;
     elseif love.keyboard.isDown("down", "s") then
         self.y = self.y + self.dy * dt;
     end
+
+    self.y = self.y + self.dy
 end
 
 function Player:render()
--- main.lua
+++ main.lua
@@ -1,6 +1,9 @@
 push = require 'push'
 Class = require 'class'
+sti = require 'libraries/sti'
+camera = require 'libraries/camera'
 require 'Player'
+sti = require 'libraries/sti'
 
 WINDOW_WIDTH = 1280
 WINDOW_HEIGHT = 720
@@ -8,21 +11,28 @@ WINDOW_HEIGHT = 720
 VIRTUAL_WIDTH = 84
 VIRTUAL_HEIGHT = 48
 
+cam_x = 0
+cam_y = 0
+
 function love.load()
     love.window.setTitle('gamejam nokia')
     love.graphics.setDefaultFilter("nearest", "nearest")
 
     math.randomseed(os.time())
-    player = Player(10, 10, 8, 8);
+    player = Player(44, 22, 8, 8);
 
     smallFont = love.graphics.newFont('Ark.ttf', 8)
     love.graphics.setFont(smallFont)
-        
+
     push:setupScreen(VIRTUAL_WIDTH, VIRTUAL_HEIGHT, WINDOW_WIDTH, WINDOW_HEIGHT, {
         fullscreen = false,
         resizable = true,
-        vsync = true
+        vsync = true,
+        pixelperfect = true,
     })
+
+    gameMap = sti('maps/map_one.lua')
+    cam = camera()
 
     gameState = 'main_menu'
 end
@@ -31,42 +41,38 @@ function love.resize(w, h)
     push:resize(w, h)
 end
 
+function getViewpointForCamera()
+    coordsx = math.floor(push._RWIDTH/2 - VIRTUAL_WIDTH/2 + player.x)
+    coordsy = math.floor(push._RHEIGHT/2 - VIRTUAL_HEIGHT/2 + player.y) --push._RWIDTH/2, push._RHEIGHT/2
+    return coordsx, coordsy
+end
 
 function love.update(dt)
     player:update(dt)
+    cam:lookAt(getViewpointForCamera())
+    -- print(cam:position())
 end
 
---[[
-    A callback that processes key strokes as they happen, just the once.
-    Does not account for keys that are held down, which is handled by a
-    separate function (`love.keyboard.isDown`). Useful for when we want
-    things to happen right away, just once, like when we want to quit.
-]]
 function love.keypressed(key)
     if gameState == 'main_menu' then
         if key == "return" then
             gameState = 'game_loop'
-        end
-    elseif gameState == 'game_loop' then
-        if key == 'left' or key == 'a' then
-        elseif key == 'right' or key == 'd' then
-        elseif key == 'w' or key == 'up' then
-        elseif key == 'space' then
         end
     end
 end
 
 function love.draw()
     push:apply('start')
+    cam:attach()
+        gameMap:drawLayer(gameMap.layers["Tile Layer 1"])
 
-    love.graphics.clear(40/255, 45/255, 52/255, 255/255)
-    
-    if gameState == 'main_menu' then
-        love.graphics.setFont(smallFont)
-        love.graphics.printf('Press Enter :)', 0, 20, VIRTUAL_WIDTH, 'center')
-    elseif gameState == 'game_loop' then
-        player:render()
-    end
-    
+
+        if gameState == 'main_menu' then
+            love.graphics.setFont(smallFont)
+            love.graphics.printf('Press Enter :)', 0, 20, VIRTUAL_WIDTH, 'center')
+        elseif gameState == 'game_loop' then
+            player:render()
+        end
+    cam:detach()
     push:apply('end')
 end
